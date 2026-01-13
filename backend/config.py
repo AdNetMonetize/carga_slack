0faa18a834dada7a3e6769ab72ce2a61490585bd
root <root@srv815260.hstgr.cloud>
2026-01-13 18:47:07 -0300

    adjust

--- backend/config.py
+++ backend/config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Slack Configuration
-SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN', 'seu_token_slack_aqui')
-SLACK_CHANNEL = os.getenv('SLACK_CHANNEL', 'seu_canal_slack_aqui')
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
+SLACK_CHANNEL = os.getenv('SLACK_CHANNEL')
 
 # Google Sheets Configuration
-GOOGLE_SHEETS_URL = os.getenv('GOOGLE_SHEETS_URL', 'https://docs.google.com/spreadsheets/d/1tE7ZBhvsfUqcZNa4UnrrALrXOwRlc185a7iVPh_iv7g/edit?gid=1046712131')
+GOOGLE_SHEETS_URL = os.getenv('GOOGLE_SHEETS_URL')
 
 # File paths
 PROCESSED_DATA_FILE = 'data/processed_records.json'
